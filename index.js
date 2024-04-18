27f8e19258702411eb47297e56af61c5982d47c4
xavier <villax989@icloud.com>
2024-04-18 14:05:46 -0400

    finished

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-beforeEach(function () {
-  cats.length = 0;
+const cats=["Milo", "Otis", "Garfield",]
 
-  cats.push("Milo", "Otis", "Garfield");
-});
+function destructivelyAppendCat(name){
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+
+function appendCat(name){
+  //return[...cats, name]
+const newCatsArray = [...cats]  
+newCatsArray.push(name)
+return newCatsArray
+}
+
+function prependCat(name){
+  //return[name, ...cats]
+const newCatsArray = [...cats]
+newCatsArray.unshift(name)
+return newCatsArray
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
